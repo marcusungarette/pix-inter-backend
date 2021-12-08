ab5da8538e753b053c28571c0d120e3a8c18d7ac
marcusungarette <marcusungarette@gmail.com>
2021-12-08 18:51:22 -0300

    Chore-Started pixRoutes, adding service and controllers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import pixRouter from './pix.routes';
 import userRouter from './user.routes';
 
 const routes = Router();
 
 routes.use('/user', userRouter);
+routes.use('/pix', pixRouter);
 
 export default routes;
